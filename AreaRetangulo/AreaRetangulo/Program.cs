d01369c25ea8621a6617e6aa5c55292ef5e12802
Felipe Murador <106840186+FelipeMurador@users.noreply.github.com>
2024-11-24 18:08:04 -0300

    Add files via upload

---
+++ AreaRetangulo/AreaRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+           Console.WriteLine("Calcula a area de um retangulo");
+            Console.WriteLine("Informe a altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a base /largura: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            retangulo.ExibeDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AreaRetangulo/AreaRetangulo/Retangulo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Retangulo
+
+    {
+        //construtor 
+        public Retangulo()
+        {
+            this.Altura = 0;
+            this.Base = 0;
+        }
+        //propriedade
+        private Double baseret;
+
+        public Double Base
+        {
+            get { return baseret; }
+            set {
+                if (value >= 0) baseret = value;
+                else baseret = 0;
+            }
+
+        }
+        //altura do retangulo
+        private Double altret;
+
+        public Double Altura
+        {
+            get { return altret; }
+            set 
+            { 
+                if (value>0) altret = value;
+                else altret = 0;
+            }
+        }
+        public Double Area
+        {
+            get { return altret * baseret;  }
+          
+        }
+        public void  ExibeDados()
+        {
+            Console.WriteLine("Base: " + this.Base);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Area: " + this.Area);
+        }
+
+    }
+}
